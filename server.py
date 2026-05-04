7fd7b077afc0405ef32b2cdeab7f77e2051bf1ba
Ananaz4425 <poiasniucana@gmail.com>
2026-05-03 20:26:52 -0400

    Add header comments and improve formatting in server.py

--- server.py
+++ server.py
@@ -214,3 +214,6 @@ if __name__ == "__main__":
     init_db()
     print("Open http://localhost:5000 in your browser")
     app.run(debug=True, port=5000)
+else:
+    # This runs on PythonAnywhere
+    init_db()
